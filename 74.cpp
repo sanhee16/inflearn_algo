7922400cdb7a2b74ddfb61f0620d3067780eca23
sanhee16 <43190572+sanhee16@users.noreply.github.com>
2020-09-04 13:05:37 +0900

    Create 74.cpp

---
+++ 74.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	priority_queue<int, vector<int>, greater<int> > pq;
+	int inp;
+	while(true){
+		cin >> inp;
+		if(inp==-1)
+			break;
+		if(inp==0){
+			if(pq.empty()==true)
+				cout << -1 <<endl;
+			else{
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}else{
+			pq.push(inp);
+		}
+	}
+	return 0;
+}
+
